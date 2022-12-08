05c85759337015e84e614f5fec37bb14c4d4be48
unknown <guillaume.stuyck@gmail.com>
2022-12-08 14:54:40 +0100

    belle avancée du jeudi

--- src/Controller/AnnonceController.php
+++ src/Controller/AnnonceController.php
@@ -6,9 +6,11 @@ use App\Entity\Annonce;
 use App\Form\AnnonceType;
 use App\Repository\AnnonceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 #[Route('/annonce')]
 class AnnonceController extends AbstractController
@@ -22,18 +24,36 @@ class AnnonceController extends AbstractController
     }
 
     #[Route('/new', name: 'app_annonce_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, AnnonceRepository $annonceRepository): Response
+    public function new(Request $request, AnnonceRepository $annonceRepository, SluggerInterface $slugger): Response
     {
+        $author = $this->getUser();
         $annonce = new Annonce();
         $form = $this->createForm(AnnonceType::class, $annonce);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $imageFile = $form->get('imgfile')->getData();
+
+            if ($imageFile) {
+                $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename . '-' . uniqid() . '.' . $imageFile->guessExtension();
+                try {
+                    $imageFile->move(
+                        $this->getParameter('images_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+                $annonce->setImgfile($newFilename);
+            }
+            $annonce->setAuthor($author);
+            $annonce->setIsVisible(true);
             $annonceRepository->save($annonce, true);
-
-            return $this->redirectToRoute('app_annonce_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('home', [], Response::HTTP_SEE_OTHER);
         }
-
         return $this->renderForm('annonce/new.html.twig', [
             'annonce' => $annonce,
             'form' => $form,
@@ -69,7 +89,7 @@ class AnnonceController extends AbstractController
     #[Route('/{id}', name: 'app_annonce_delete', methods: ['POST'])]
     public function delete(Request $request, Annonce $annonce, AnnonceRepository $annonceRepository): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$annonce->getId(), $request->request->get('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $annonce->getId(), $request->request->get('_token'))) {
             $annonceRepository->remove($annonce, true);
         }
 
--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Marque;
 use App\Repository\AnnonceRepository;
+use App\Repository\MarqueRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,10 +12,22 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(AnnonceRepository $annonceRepository): Response
+    public function index(AnnonceRepository $annonceRepository, MarqueRepository $marqueRepository): Response
     {
         return $this->render('main/index.html.twig', [
-            'annonces' => $annonceRepository->findAll()
+            'annonces' => $annonceRepository->findAll(),
+            'marques' => $marqueRepository->findAll(),
+
+        ]);
+    }
+
+    #[Route('/tab/{id}', name: 'app_marque_tab', methods: ['GET'])]
+    public function tab(Marque $marque, MarqueRepository $marqueRepository, AnnonceRepository $annonceRepository): Response
+    {
+        return $this->render('tab.html.twig', [
+            'brand' => $marque,
+            'marques' => $marqueRepository->findAll(),
+            'annonces' => $annonceRepository->findAll(),
         ]);
     }
 }
